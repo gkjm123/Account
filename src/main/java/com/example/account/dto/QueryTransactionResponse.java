a1da0dafb72f06ffca4cbc1027290ef282c4f8c5
nuni <gkje123@naver.com>
2024-05-27 18:57:57 +0900

    first commit

---
+++ src/main/java/com/example/account/dto/QueryTransactionResponse.java
@@ -0,0 +1,34 @@
+package com.example.account.dto;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QueryTransactionResponse {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private String transactionId;
+    private Long amount;
+    private LocalDateTime transactionAt;
+
+    public static QueryTransactionResponse from(TransactionDto transactionDto) {
+        return QueryTransactionResponse.builder()
+                .accountNumber(transactionDto.getAccountNumber())
+                .transactionType(transactionDto.getTransactionType())
+                .transactionAt(transactionDto.getTransactedAt())
+                .amount(transactionDto.getAmount())
+                .transactionResultType(transactionDto.getTransactionResultType())
+                .transactionId(transactionDto.getTransactionId())
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/account/dto/TransactionDto.java
@@ -0,0 +1,42 @@
+package com.example.account.dto;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.Transaction;
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDto {
+
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private Long amount;
+    private Long balanceSnapshot;
+    private String transactionId;
+    private LocalDateTime transactedAt;
+
+    public static TransactionDto fromEntity(Transaction transaction) {
+        return TransactionDto.builder()
+                .accountNumber(transaction.getAccount().getAccountNumber())
+                .transactionType(transaction.getTransactionType())
+                .transactionResultType(transaction.getTransactionResultType())
+                .amount(transaction.getAmount())
+                .balanceSnapshot(transaction.getBalanceSnapshot())
+                .transactedAt(transaction.getTransactedAt())
+                .transactionId(transaction.getTransactionId())
+                .build();
+    }
+
+}
